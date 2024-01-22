dfdce2a7cf3c2fdf6034c13bbb23c4789cfd9a50
Adam Guest <agu@copenhagensoftware.com>
2024-01-22 08:27:20 +0100

    Add empty nextjs project for frontend

---
+++ Frontend/vacation-days/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-between p-24">
+        <h1>Vacation Days</h1>
+        <p>
+            It would be cool if I could click a button here and find out
+            how many vacation days I had...
+        </p>
+    </main>
+  )
+}
